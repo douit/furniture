adfd7b07182ff270307fd35d9428045aa269585f
AleixGallimo <a969803965@qq.com>
2020-01-19 17:29:01 +0800

    配置

---
+++ oauth-server/src/main/java/com/qf/furniture/oauth/server/configuration/WebSecurityConfig.java
@@ -0,0 +1,32 @@
+package com.qf.furniture.oauth.server.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+/**
+ * @author CHAN
+ * @date 2020/01/19
+ */
+@Configuration
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true, securedEnabled = true, jsr250Enabled = true)
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Bean
+    public BCryptPasswordEncoder bCryptPasswordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication()
+                .withUser("admin").password(bCryptPasswordEncoder().encode("admin")).roles("ADMIN")
+                .and()
+                .withUser("user").password(bCryptPasswordEncoder().encode("user")).roles("USER");
+    }
+}
